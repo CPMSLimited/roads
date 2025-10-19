1ade1d24c11fa7c275282631070d08b819abf8dc
alkadelik <debola_adeola@yahoo.com>
2025-10-19 08:28:01 +0100

    fixing

--- roads/settings.py
+++ roads/settings.py
@@ -41,7 +41,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://www.cpmsferma.com",
 ]
 
-CORS_ALLOW_ALL_ORIGINS: True
+CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:8080',
 ]
