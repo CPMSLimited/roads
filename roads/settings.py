bde8041135ffa11e646cbf547a4327f4e4eaf6a6
alkadelik <debola_adeola@yahoo.com>
2025-10-27 16:34:36 +0100

    update seettings

--- roads/settings.py
+++ roads/settings.py
@@ -11,8 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 
-# DEBUG = config('DEBUG')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = [
     "cpmsferma.com",
@@ -141,10 +140,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-# STATICFILES_FINDERS = [
-#     'django.contrib.staticfiles.finders.FileSystemFinder',
-#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-# ]
 
 STATIC_URL = 'static/'
 
